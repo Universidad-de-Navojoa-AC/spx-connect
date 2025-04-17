23b9ff0a616707cc0dce5bd93de9925b1f5581df
Somar Romero <somar.dev@icloud.com>
2025-04-17 11:22:44 -0700

    ✨ feat: se añadió sunplusAccounts() a SpxClientInterface para acceso desde la Facade

--- src/Contracts/SpxClientInterface.php
+++ src/Contracts/SpxClientInterface.php
@@ -3,6 +3,7 @@
 namespace Unav\SpxConnect\Contracts;
 
 use Unav\SpxConnect\Services\AuthService;
+use Unav\SpxConnect\Services\SunPlusAccountService;
 
 /**
  * Interface SpxClientInterface
@@ -14,4 +15,6 @@ interface SpxClientInterface
     public function getService(string $key): mixed;
 
     public function auth(): AuthService;
+
+    public function sunplusAccounts(): SunPlusAccountService;
 }
