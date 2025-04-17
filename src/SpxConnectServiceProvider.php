76241285f1863e4342c0165e239bc479f600e087
Somar Romero <somar.dev@icloud.com>
2025-04-17 11:26:45 -0700

    ✨ feat: se añadió sunplusAccounts al binding de SpxClient

--- src/SpxConnectServiceProvider.php
+++ src/SpxConnectServiceProvider.php
@@ -5,6 +5,7 @@ namespace Unav\SpxConnect;
 use Illuminate\Support\ServiceProvider;
 use Unav\SpxConnect\Contracts\SpxClientInterface;
 use Unav\SpxConnect\Services\AuthService;
+use Unav\SpxConnect\Services\SunPlusAccountService;
 
 class SpxConnectServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,7 @@ class SpxConnectServiceProvider extends ServiceProvider
         $this->app->bind(SpxClientInterface::class, function ($app) {
             return new SpxClient([
                 'auth' => $app->make(AuthService::class),
+                'sunplusAccounts' => $app->make(SunPlusAccountService::class),
             ]);
         });
     }
