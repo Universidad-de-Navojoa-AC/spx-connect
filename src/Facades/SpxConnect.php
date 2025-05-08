90ebcd72b9be6f8f62ab51f409eacc10da16ac31
Somar Romero <somar.dev@icloud.com>
2025-05-08 16:22:10 -0700

    chore: Agregar PHPDoc al facade con los métodos declarados

--- src/Facades/SpxConnect.php
+++ src/Facades/SpxConnect.php
@@ -3,7 +3,23 @@
 namespace Unav\SpxConnect\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Unav\SpxConnect\Contracts\CacheManagerInterface;
+use Unav\SpxConnect\Services\AuthService;
+use Unav\SpxConnect\Services\EducationLevelService;
+use Unav\SpxConnect\Services\ProductService;
+use Unav\SpxConnect\Services\SunPlusAccountService;
+use Unav\SpxConnect\Services\SunPlusDimensionService;
 
+/**
+ * Facade for SpxClient
+ *
+ * @method static AuthService auth()
+ * @method static CacheManagerInterface cache()
+ * @method static SunPlusAccountService sunplusAccounts()
+ * @method static SunPlusDimensionService sunplusDimension()
+ * @method static ProductService products()
+ * @method static EducationLevelService educationLevels()
+ */
 class SpxConnect extends Facade
 {
     protected static function getFacadeAccessor(): string
