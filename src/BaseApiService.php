f189e4cbebf0cd480e0fee34f4540c85b93d33a3
Somar Romero <somar.dev@icloud.com>
2026-03-02 12:43:53 -0700

    feat(BaseApiService): Mejorar handling de peticiones, timeout y refresco de token.

--- src/BaseApiService.php
+++ src/BaseApiService.php
@@ -2,6 +2,8 @@
 
 namespace Unav\SpxConnect;
 
+use Illuminate\Contracts\Cache\LockTimeoutException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Unav\SpxConnect\Services\AuthService;
 use Unav\SpxConnect\Services\TokenManager;
@@ -18,15 +20,24 @@ class BaseApiService
         $this->auth = new AuthService();
     }
 
+    /**
+     * @throws LockTimeoutException
+     */
     public function request(string $method, string $endpoint, array $data = [], array $headers = [])
     {
-        $response = Http::withToken(TokenManager::getToken($this->userId))
+        $client = fn() => Http::withToken(TokenManager::getToken($this->userId))
             ->withHeaders($headers)
-            ->$method("$this->baseUrl/$endpoint", $data);
+            ->connectTimeout(10)
+            ->timeout(90);
 
-        if ($response->status() === 401 && $this->auth->refreshAccessToken()) {
-            $response = Http::withToken(TokenManager::getToken($this->userId))
-                ->$method("$this->baseUrl/$endpoint", $data);
+        $response = $client->$method("$this->baseUrl/$endpoint", $data);
+
+        if ($this->isAuthFailure($response)) {
+            Cache::lock("token-refresh-{$this->userId}", 10)->block(5, function () {
+                $this->auth->refreshAccessToken();
+            });
+
+            $response = $client()->$method("$this->baseUrl/$endpoint", $data);
         }
 
         return $response;
@@ -41,5 +52,19 @@ class BaseApiService
     {
         $this->userId = $userId;
         return $this;
+    }
+
+    private function isAuthFailure($response): bool
+    {
+        if ($response->status() === 401) {
+            return true;
+        }
+
+        if ($response->status() === 404) {
+            $message = data_get($response->json(), 'message');
+            return $message === 'Unauthenticated.';
+        }
+
+        return false;
     }
 }
