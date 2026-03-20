92b9a734becd0a305e94f1e531a5fc28ad6729b9
Somar Romero <somar.dev@icloud.com>
2026-03-20 10:31:31 -0700

    feat(SunPlusDimensionService): Añadir método para creación de dimensiones de Sunplus

--- src/Services/SunPlusDimensionService.php
+++ src/Services/SunPlusDimensionService.php
@@ -2,6 +2,7 @@
 
 namespace Unav\SpxConnect\Services;
 
+use Illuminate\Contracts\Cache\LockTimeoutException;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Log;
@@ -28,6 +29,45 @@ class SunPlusDimensionService extends BaseApiService
             $body = $e->response->body();
 
             Log::error('Error al obtener la lista de dimensiones de SunPlus', [
+                'status' => $status,
+                'body' => $body,
+                'ex' => $e,
+            ]);
+        } catch (ConnectionException $e) {
+            Log::error('Conexión fallida a SunPlusXtra', ['ex' => $e]);
+        }
+
+        return [];
+    }
+
+    /**
+     * Create a new dimension on Sunplus
+     *
+     * @param DimensionType $dimension
+     * @param string $code
+     * @param string $lookup
+     * @param string $name
+     * @param int|null $prohibitPosting
+     * @return array
+     * @throws LockTimeoutException
+     */
+    public function create(DimensionType $dimension, string $code, string $lookup, string $name, ?int $prohibitPosting = 0): array
+    {
+        try {
+            return $this->request('post', 'dimension', [
+                'catId' => $dimension->value,
+                'code' => $code,
+                'lookup' => $lookup,
+                'name' => $name,
+                'prohibitPosting' => $prohibitPosting,
+            ])
+                ->throw()
+                ->json('response', []);
+        } catch (RequestException $e) {
+            $status = $e->response->status();
+            $body = $e->response->body();
+
+            Log::error('Error al crear la dimensión en SunPlus', [
                 'status' => $status,
                 'body' => $body,
                 'ex' => $e,
